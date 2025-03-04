7f290c15a8af1a81160625d895273baa21f7c4d5
Jitesh Kumar <jiteshsaini2005@gmail.com>
2025-03-05 00:23:11 +0530

    first commit23

--- src/Component/QuoteGenerator.jsx
+++ src/Component/QuoteGenerator.jsx
@@ -17,7 +17,7 @@ function QuoteGenerator() {
 
   const fetchQuote = async () => {
     try {
-      const response = await fetch("http://api.quotable.io/random/");
+      const response = await fetch("https://api.quotable.io/random/");
       
       
       if (!response.ok) {  
